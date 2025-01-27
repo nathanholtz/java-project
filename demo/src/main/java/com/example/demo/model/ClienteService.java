fa7aaf5b3b3bb0fb50b804100182fe7c85c509b8
HOLTZ <nathan.holtz0805@gmail.com>
2025-01-27 19:22:18 -0300

    demo/.gitattributes 	demo/.gitignore 	demo/.mvn/ 	demo/mvnw 	demo/mvnw.cmd 	demo/pom.xml 	demo/src/main/java/com/example/demo/ProjetoApplication.java 	demo/src/main/java/com/example/demo/ServletInitializer.java 	demo/src/main/java/com/example/demo/controller/ 	demo/src/main/java/com/example/demo/model/Cliente.java 	demo/src/main/java/com/example/demo/model/ClienteDAO.java 	demo/src/main/resources/application.yaml 	demo/src/test/

---
+++ demo/src/main/java/com/example/demo/model/ClienteService.java
@@ -0,0 +1,13 @@
+package com.example.demo.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteService {
+    @Autowired 
+    ClienteDAO cdao;
+    public void inserirCliente(Cliente c){
+        cdao.inserirCliente(c);
+    }  
+}
